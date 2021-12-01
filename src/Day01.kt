25188577d46bc80d45cd563f0c655085227cd98c
jaavv <mjaavv@gmail.com>
2021-12-01 12:50:48 -0700

    Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val result = input.map { it.toInt() }
+        var prev: Int = -1
+        var next: Int?
+        var count = 0
+        for (i in result) {
+            next = i
+            if (next > prev) count++
+            prev = next
+        }
+        return count - 1
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val result2 = input.map { it.toInt() }
+        val x = result2.windowed(3)
+        var prev2: Int = -1
+        var next2: Int?
+        var count = 0
+        for (i in x) {
+            next2 = i.sum()
+            if (next2 > prev2) count++
+            prev2 = next2
+        }
+        return count - 1
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
