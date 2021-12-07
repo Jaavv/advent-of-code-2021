f06a9cb5d4ea14e771e7390fa55441f5a8bd803d
jaavv <mjaavv@gmail.com>
2021-12-07 16:10:15 -0700

    day05 solutions

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,66 +1,39 @@
+import kotlin.math.abs
+
 // https://adventofcode.com/2021/day/5
 
 fun main() {
     val input = readInput("Day05")
-    println("PART1 POINTS: ${day5part1(input)}")
+    println("PART1 POINTS: ${day5part1(input)}") // 4421
+    println("PART2 POINTS: ${day5part2(input)}") // 18674
 }
 
 fun day5part1(input: List<String>): Int {
-    val coordinates = input.map { inputParse(it) }.filter { straightLine(it) }
-    val maxXX = coordinates.map { largestCoord(it) }.maxOf { it.x }
-    val maxYY = coordinates.map { largestCoord(it) }.maxOf { it.y }
-    val graph: MutableList<MutableList<Int>> = mutableListOf((mutableListOf()))
-    var maxX = 0
-    var maxY = 0
-    for (i in input) {
-        val data = inputParse(i)
-        if (straightLine(data)) {
-            val lines = line(data)
-            val lmaxX = lines.maxOfOrNull { it.x }
-            val lmaxY = lines.maxOfOrNull { it.y }
-//            if (maxX < lmaxX!!) maxX = lmaxX
-//            if (maxY < lmaxY!!) maxY = lmaxY
-//            graph.addAll()
+    val data = input.map { inputParse(it) }.filter { isStraightLine(it) }
+    val maxX = data.map { largestCoord(it) }.maxOf { it.x }
+    val maxY = data.map { largestCoord(it) }.maxOf { it.y }
+    val graph = MutableList(maxY + 1) { MutableList(maxX + 1) { 0 } }
+    val coordinates = data.map { straightLine(it) }.flatten()
+    for (i in coordinates) insertLine(graph, i)
+    return graph.flatten().count { it >= 2 }
+}
 
-
-            if (graph.size < lmaxY!!) {
-                val diffY = lmaxY - graph.size
-                expandColumn(graph, diffY, 1 + lmaxX!!)
-//                graph.addAll(MutableList(diffY) { MutableList(lmaxX!!) { 0 } })
-                maxY = lmaxY
-            }
-            val currentRow = graph.map { it.size }.maxOf { it }
-            if (currentRow < lmaxX!!) {
-                val diffX = lmaxX - currentRow
-                for (row in graph) expandRow(row, diffX)
-                maxX = lmaxX
-            }
-//            for (row in graph) {
-//                if (row.size < maxX!!) {
-//                    val diffX = maxX - row.size + 1
-//                    for (i in row.size + 1..maxX + 1)
-//                        row.add(0)
-//                }
-//            }
-            for (l in lines) {
-                try {
-                    graph[l.y][l.x] += 1
-                } catch (e: Exception) {
-                    println(data)
-                    println("COLUMNS: ${graph.size}, ROWS: ${graph[0].size}")
-                    println(graph[l.y])
-                    break
-                }
-            }
-        }
-    }
-
+fun day5part2(input: List<String>): Int {
+    val straightLine =
+        input.map { inputParse(it) }.filter { isStraightLine(it) }.map { straightLine(it) }.flatten().toMutableList()
+    val diagLine =
+        input.map { inputParse(it) }.filter { isDiagonalLine(it) }.map { diagonalLine(it) }.flatten().toMutableList()
+    straightLine.addAll(diagLine)
+    val maxX = straightLine.map { largestCoord2(it) }.maxOf { it.x }
+    val maxY = straightLine.map { largestCoord2(it) }.maxOf { it.y }
+    val graph = MutableList(maxY + 1) { MutableList(maxX + 1) { 0 } }
+    for (i in straightLine) insertLine(graph, i)
     return graph.flatten().count { it >= 2 }
 }
 
 data class Coordinate(val x: Int, val y: Int)
 data class Input(val from: Coordinate, val to: Coordinate)
-enum class Direction { HORIZONTAL, VERTICAL }
+enum class Direction { HORIZONTAL, VERTICAL, DIAG_UP_FORWARD, DIAG_UP_BACKW, DIAG_DOWN_FORWARD, DIAG_DOWN_BACKW }
 
 fun inputParse(input: String): Input {
     val coord = input.split(" -> ")
@@ -75,28 +48,32 @@ fun largestCoord(input: Input): Coordinate {
     return Coordinate(maxOf(input.from.x, input.to.x), maxOf(input.from.y, input.to.y))
 }
 
-fun straightLine(input: Input): Boolean {
-    return (input.from.x == input.to.x) or (input.from.y == input.to.y)
+fun largestCoord2(input: Coordinate): Coordinate {
+    return Coordinate(maxOf(input.x, input.x), maxOf(input.y, input.y))
+}
+
+fun insertLine(map: MutableList<MutableList<Int>>, coordinate: Coordinate) {
+    map[coordinate.y][coordinate.x] += 1
+}
+
+fun isStraightLine(input: Input): Boolean {
+    return (input.from.x == input.to.x) || (input.from.y == input.to.y)
+}
+
+fun isDiagonalLine(input: Input): Boolean {
+//    return ((input.from.x == input.from.y) && (input.to.x == input.to.y)) ||
+//            ((input.from.x == input.to.y) && (input.from.y == input.to.x))
+    return (abs(input.from.x - input.to.x) == abs(input.from.y - input.to.y))
 }
 
 fun direction(from: Coordinate, to: Coordinate): Direction {
     return if (from.x == to.x) Direction.VERTICAL else Direction.HORIZONTAL
 }
 
-fun expandRow(row: MutableList<Int>, amount: Int) {
-    for (i in 0..amount) row.add(0)
-}
-
-fun expandColumn(graph: MutableList<MutableList<Int>>, amount: Int, fill: Int) {
-    for (i in 0..amount) {
-        graph.add(MutableList(fill) { 0 })
-    }
-}
-
-fun line(input: Input): List<Coordinate> {
+fun straightLine(input: Input): List<Coordinate> {
     val list = mutableListOf<Coordinate>()
-    var start = 0;
-    var finish = 0;
+    var start = 0
+    var finish = 0
     var base = 0
     val direction = direction(input.from, input.to)
 
@@ -113,7 +90,30 @@ fun line(input: Input): List<Coordinate> {
             Direction.VERTICAL -> list.add(Coordinate(base, i))
             Direction.HORIZONTAL -> list.add(Coordinate(i, base))
         }
-//        list.add(Coordinate(i, base))
+    }
+    return list
+}
+
+fun diagDirection(input: Input): Direction {
+    var diagDir: Direction = Direction.DIAG_UP_FORWARD
+    if ((input.from.y > input.to.y) && (input.from.x < input.to.x)) diagDir = Direction.DIAG_UP_FORWARD
+    if ((input.from.y > input.to.y) && (input.from.x > input.to.x)) diagDir = Direction.DIAG_UP_BACKW
+    if ((input.from.y < input.to.y) && (input.from.x < input.to.x)) diagDir = Direction.DIAG_DOWN_FORWARD
+    if ((input.from.y < input.to.y) && (input.from.x > input.to.x)) diagDir = Direction.DIAG_DOWN_BACKW
+    return diagDir
+}
+
+fun diagonalLine(input: Input): List<Coordinate> {
+    val list = mutableListOf<Coordinate>()
+    val range = abs(input.from.x - input.to.x)
+
+    for (i in 0..range) {
+        when (diagDirection(input)) {
+            Direction.DIAG_UP_FORWARD -> list.add(Coordinate(input.from.x + i, input.from.y - i))
+            Direction.DIAG_UP_BACKW -> list.add(Coordinate(input.from.x - i, input.from.y - i))
+            Direction.DIAG_DOWN_FORWARD -> list.add(Coordinate(input.from.x + i, input.from.y + i))
+            Direction.DIAG_DOWN_BACKW -> list.add(Coordinate(input.from.x - i, input.from.y + i))
+        }
     }
     return list
 }
